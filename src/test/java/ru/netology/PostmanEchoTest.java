e872d281aa4e856903fbdeac1665c52f1d1d25b0
Pridius <darya.pridius@yandex.ru>
2024-08-04 15:52:21 +0300

    fix the request

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -15,7 +15,7 @@ class PostmanEchoTest {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("Скоро зима") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("Скоро осень") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
