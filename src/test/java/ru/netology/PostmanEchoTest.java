16432b6d1687ed3cdc6468b11ea47c4c934bec6f
Pridius <darya.pridius@yandex.ru>
2024-08-04 15:48:59 +0300

    send the request and drop the test

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,31 @@
+package ru.netology;
+
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSomeData() {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Скоро зима") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Скоро осень"));
+        ;
+    }
+
+
+
+}
